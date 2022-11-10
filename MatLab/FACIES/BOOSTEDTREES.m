7b1b9e0541e702ff350697fb993196cf52c05c6f
saadallahham <saad.allahham@hotmail.com>
2022-11-10 18:58:10 -0300

    Update BOOSTEDTREES.m

--- MatLab/FACIES/BOOSTEDTREES.m
+++ MatLab/FACIES/BOOSTEDTREES.m
@@ -48,7 +48,6 @@ trainedClassifier.predictFcn = @(x) ensemblePredictFcn(predictorExtractionFcn(x)
 % Add additional fields to the result struct
 trainedClassifier.RequiredVariables = {'DENSITY', 'GAMMARAY', 'NEUTRON', 'SONICO'};
 trainedClassifier.ClassificationEnsemble = classificationEnsemble;
-trainedClassifier.About = 'This struct is a trained model exported from Classification Learner R2019a.';
 trainedClassifier.HowToPredict = sprintf('To make predictions on a new table, T, use: \n  yfit = c.predictFcn(T) \nreplacing ''c'' with the name of the variable that is this struct, e.g. ''trainedModel''. \n \nThe table, T, must contain the variables returned by: \n  c.RequiredVariables \nVariable formats (e.g. matrix/vector, datatype) must match the original training data. \nAdditional variables are ignored. \n \nFor more information, see <a href="matlab:helpview(fullfile(docroot, ''stats'', ''stats.map''), ''appclassification_exportmodeltoworkspace'')">How to predict using an exported model</a>.');
 
 % Extract predictors and response
